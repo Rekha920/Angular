197dad0d0d9fe8797e2f03a88d519f46bae76707
Your Name <rekha.dodani@emorphis.in>
2023-05-18 12:20:23 +0530

    section 3 started

--- angular-feature/src/app/app.module.ts
+++ angular-feature/src/app/app.module.ts
@@ -3,21 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { FormsModule} from '@angular/forms'
 import { AppComponent } from './app.component';
-import { ServerComponent } from './Server/server.component';
-import { ServersComponent } from './servers/servers.component';
-import { WarningAlertComponent } from './warning-alert/warning-alert.component';
-import { SuccessAlertComponent } from './success-alert/success-alert.component';
-import { Assignment2Component } from './assignment2/assignment2.component';
-import { Assignment3Component } from './assignment3/assignment3.component';
+
 @NgModule({
   declarations: [
     AppComponent,
-    ServerComponent,
-    ServersComponent,
-    WarningAlertComponent,
-    SuccessAlertComponent,
-    Assignment2Component,
-    Assignment3Component
   ],
   imports: [
     BrowserModule,
